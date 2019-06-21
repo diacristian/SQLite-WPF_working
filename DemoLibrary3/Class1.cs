20b9231d040f6a1a6d37b35aca9873edf2588abb
Cristian Diac <diac.cristian@hotmail.com>
2019-06-21 20:07:54 +0300

    Add project files.

---
+++ DemoLibrary3/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DemoLibrary3
+{
+    public class Class1
+    {
+    }
+}
---
+++ trySQLiteAndWPF/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using DemoLibraryN;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace trySQLiteAndWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<PersonModel> people = new List<PersonModel>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            LoadPeopleList();
+        }
+
+        private void LoadPeopleList()
+        {
+            // todo - get real people here
+            //people.Add(new PersonModel { FirstName = "Tim", LastName = "Corey" });
+            //people.Add(new PersonModel { FirstName = "John", LastName = "Doe" });
+            //people.Add(new PersonModel { FirstName = "Mary", LastName = "Smith" });
+
+            people = SqliteDataAccess.LoadPeople();
+
+            WireUpPeopleList();
+
+        }
+
+        private void WireUpPeopleList()
+        {
+            listPeopleListBox.ItemsSource = null;
+            listPeopleListBox.ItemsSource = people;
+        }
+
+        private void Button_Click_Add(object sender, RoutedEventArgs e)
+        {
+
+            PersonModel p = new PersonModel();
+
+            p.FirstName = firstNameText.Text;
+            p.LastName = lastNameText.Text;
+
+            //todo - do something with this item
+            //people.Add(p);
+            //WireUpPeopleList();
+
+            SqliteDataAccess.SavePerson(p);
+
+            WireUpPeopleList();
+
+            firstNameText.Text = "";
+            lastNameText.Text = "";
+
+
+        }
+
+        private void Button_Click_Refresh(object sender, RoutedEventArgs e)
+        {
+            people = SqliteDataAccess.LoadPeople();
+
+            WireUpPeopleList();
+        }
+    }
+}
